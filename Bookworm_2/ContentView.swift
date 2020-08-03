62133728afd3741be6b4abe2539f59fa75bb0a6c
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-03 13:46:36 +0530

    added extra functionality

--- Bookworm_2/ContentView.swift
+++ Bookworm_2/ContentView.swift
@@ -10,14 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment (\.managedObjectContext) var moc
-    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true)
+    ]) var books: FetchedResults<Book>
     @State private var showingAddScreen = false
+    
+    func deleteBook(at offsets: IndexSet) {
+        print("offsets => ", offsets)
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        try? moc.save()
+    }
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(books, id: \.self) { book in
-                    NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+                    NavigationLink(destination: DetailView(book: book)) {
                         EmojiRatingView(rating: book.rating)
                             .font(.largeTitle)
                         VStack(alignment: .leading) {
@@ -28,9 +40,10 @@ struct ContentView: View {
                         }
                     }
                 }
+            .onDelete(perform: deleteBook)
             }
             .navigationBarTitle("Bookworm")
-            .navigationBarItems(trailing: Button(action: {
+            .navigationBarItems(leading: EditButton(),trailing: Button(action: {
                 self.showingAddScreen.toggle()
             }) {
                 Image(systemName: "plus")
---
+++ Bookworm_2/DetailView.swift
@@ -0,0 +1,79 @@
+//
+//  DetailView.swift
+//  Bookworm_2
+//
+//  Created by Subhrajyoti Chakraborty on 03/08/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import CoreData
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    let book: Book
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        // uncomment this line if you want to make the deletion permanent
+        // try? self.moc.save()
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+                Text(self.book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text(self.book.review ?? "No review")
+                    .padding()
+                RatingView(rating: .constant(Int(self.book.rating)))
+                    .font(.largeTitle)
+            }
+        }
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                self.deleteBook()
+                }, secondaryButton: .cancel()
+            )
+        }
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+            .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
